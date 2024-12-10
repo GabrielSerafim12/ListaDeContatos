baeb2c7e4f4a1f11c8f067f51dcc00f0f6b645c9
GabrielSerafim12 <gserafim794@gmail.com>
2024-12-10 18:42:36 -0300

    enviando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ListaDeContatos from './components/Lista_de_contatos'
+
+function App() {
+
+
+  return (
+    <>
+     <ListaDeContatos  />
+    </>
+  )
+}
+
+export default App
